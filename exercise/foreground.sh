fbdd81a1234a2bc59fb0f01d81f060cce8ff1710
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-02 20:15:49 +0300

    use explicit istio version

--- exercise/foreground.sh
+++ exercise/foreground.sh
@@ -1,6 +1,4 @@
 launch.sh
-curl -L https://istio.io/downloadIstio | sh -
-
-export PATH=$HOME/istio-1.7.0/bin:$PATH
-
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.0 sh -
+export PATH=$HOME/istio-1.6.0/bin:$PATH
 istioctl install --set profile=demo
