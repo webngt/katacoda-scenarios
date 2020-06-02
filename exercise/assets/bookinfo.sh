c06ac1e9ccf6e6af3e35cb8405cf0b3d82599f7c
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-02 21:32:33 +0300

    define step 2

---
+++ exercise/assets/bookinfo.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+istio_root=$HOME/istio-1.6.0
+
+cat <<EOF | kubectl create -f -
+{
+  "apiVersion": "v1",
+  "kind": "Namespace",
+  "metadata": {
+    "name": "bookinfo",
+    "labels": {
+      "name": "bookinfo",
+      "istio-injection": "enabled"
+    }
+  }
+}
+EOF
+
+kubectl describe namespace bookinfo
+
+cat $istio_root/samples/bookinfo/platform/kube/bookinfo.yaml | kubectl -n bookinfo apply -f -
+
+cat $istio_root/samples/bookinfo/networking/bookinfo-gateway.yaml | kubectl -n bookinfo apply -f - 
+
+cat $istio_root/samples/bookinfo/networking/destination-rule-all-mtls.yaml | kubectl -n bookinfo apply -f -
+
+echo "Ingress node port: $(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')"
+
+#watch -n 0.5 curl -o /dev/null -s -w %{http_code} 192.168.64.35:$INGRESS_PORT/productpage
--- exercise/step2.md
+++ exercise/step2.md
@@ -1,7 +1,8 @@
 This is your second step.
 
-## Install istio
+## Install application
 
-1. Enable istio `microk8s enable istio`{{execute T3}}
+1. Check pods and services READY status `kubectl -n bookinfo get services && kubectl -n bookinfo get pods`{{execute}}
+1. Check gateway `kubectl -n bookinfo get gateway`{{execute}}
 1. Port forward Kiali `kubectl port-forward svc/kiali 20001:20001 -n istio-system --address 0.0.0.0`{{execute T3}}
 1. Access Kiali https://[[HOST_SUBDOMAIN]]-20001-[[KATACODA_HOST]].environments.katacoda.com
