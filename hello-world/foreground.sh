e2496ca53fcf7b0743091d739573871b14596336
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-02 13:21:58 +0300

    inject system wide aliases to .bash_profile

--- hello-world/foreground.sh
+++ hello-world/foreground.sh
@@ -3,8 +3,10 @@ git clone https://github.com/webngt/upgraded-guacamole.git
 # bootstrap microk8s cluster
 export CNLAB_HOME=$(pwd)/upgraded-guacamole
 
-# system wide aliases
+# inject system wide aliases
+cat <<EOF >> $HOME/.bash_profile
 alias kubectl='sudo microk8s kubectl'
+EOF
 
 # inject local environment to bootstrap.sh
 cat <<EOF > $CNLAB_HOME/.env_sh
@@ -15,3 +17,7 @@ alias microk8s='sudo microk8s'
 EOF
 
 $CNLAB_HOME/bootstrap.sh
+
+. $HOME/.bash_profile
+token=$(kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
+kubectl -n kube-system describe secret $token
