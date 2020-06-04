a1e985c2ba33538eb8f2ec49c7fcf6a14ab401d9
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-05 00:22:05 +0300

    iframe

--- opa/foreground.sh
+++ opa/foreground.sh
@@ -14,9 +14,3 @@ mv opa /usr/bin
 # ip address
 host_ip=$(ifconfig ens3 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 nohup code-server --bind-addr=$host_ip:8080 > /tmp/code-server.log 2>&1 </dev/null &
-
-cat <<EOF > /root/.local/share/code-server/User/settings.json
-{
-    "opa.path": "/usr/local/bin/opa"
-}
-EOF
