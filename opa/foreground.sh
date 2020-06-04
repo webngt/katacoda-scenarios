af32b5653106def16f063f87fff15c79d084306c
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-04 21:57:29 +0300

    display password and ide link

--- opa/foreground.sh
+++ opa/foreground.sh
@@ -1,2 +1,9 @@
 git clone https://github.com/EvgenyZ54/rego_junit_test
+
+# install code-server editor
 curl -fsSL https://code-server.dev/install.sh | sh
+
+# run code-server in background
+nohup code-server > /tmp/code-server.log 2>&1 </dev/null &
+
+printf '\n\nUse this password to login to ide\n---------------------------------\n\n' && cat ~/.config/code-server/config.yaml| grep password: && printf '\n'
