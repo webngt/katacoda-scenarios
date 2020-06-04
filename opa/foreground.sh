7e7c801665965b49e8f1162c8a0d2e32c000a9ac
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-05 00:12:53 +0300

    try /usr/bin

--- opa/foreground.sh
+++ opa/foreground.sh
@@ -9,7 +9,7 @@ code-server --install-extension=tsandall.opa --force
 # install opa
 curl -L -o opa https://openpolicyagent.org/downloads/latest/opa_linux_amd64
 chmod 755 opa
-mv opa /usr/local/bin
+mv opa /usr/bin
 
 # ip address
 host_ip=$(ifconfig ens3 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
