e17c41fecae7ec153db582d937b34f0a7ac23327
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-04 22:13:32 +0300

    interactively run ide

--- opa/foreground.sh
+++ opa/foreground.sh
@@ -6,4 +6,4 @@ curl -fsSL https://code-server.dev/install.sh | sh
 # run code-server in background
 nohup code-server > /tmp/code-server.log 2>&1 </dev/null &
 
-printf '\n\nUse this password to login to ide\n---------------------------------\n\n' && cat ~/.config/code-server/config.yaml| grep password: && printf '\n'
+printf '\n\nUse this password to login to ide\n---------------------------------\n\n' && cat ~/.config/code-server/config.yaml | grep password: && printf '\n'
--- opa/step1.md
+++ opa/step1.md
@@ -1,1 +1,5 @@
-Start online IDE using pssword from terminal output https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com
+Start online IDE `nohup code-server --bind-addr=[[HOST_IP]] > /tmp/code-server.log 2>&1 </dev/null &`{{execute T1}}
+
+get password `cat ~/.config/code-server/config.yaml | grep password:`{{execute T1}}
+
+login to IDE https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com
