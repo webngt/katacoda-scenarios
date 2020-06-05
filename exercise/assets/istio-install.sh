00bbcdfa5bf9540054890ebe8fccb898bf069772
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-05 23:58:34 +0300

    untaint control plane

--- exercise/assets/istio-install.sh
+++ exercise/assets/istio-install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# untaint control plane
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
 [ ! -d "$HOME/istio-1.6.0/bin" ] && curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.0 sh -
 export PATH=$HOME/istio-1.6.0/bin:$PATH
 istioctl install --set profile=demo
