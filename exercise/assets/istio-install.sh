af88b8ff79894ba5d7eaac7f58e761c3e802e50b
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-08 14:01:50 +0300

    increase rediness timeout

--- exercise/assets/istio-install.sh
+++ exercise/assets/istio-install.sh
@@ -5,7 +5,7 @@ kubectl taint nodes --all node-role.kubernetes.io/master-
 
 [ ! -d "$HOME/istio-1.6.0/bin" ] && curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.0 sh -
 export PATH=$HOME/istio-1.6.0/bin:$PATH
-istioctl install --set profile=demo
+istioctl install --set profile=demo --readiness-timeout='10m0s'
 
 kubectl -n istio-system patch service istio-ingressgateway -p "$(cat /tmp/node-port.yaml)"
 kubectl -n istio-system patch service istio-ingressgateway -p "$(cat /tmp/immutable-ports.yaml)"
