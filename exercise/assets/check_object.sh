ae42a6205163b244fd34222742632fdc6bdbe277
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-09 18:26:30 +0300

    fix msg assignment

--- exercise/assets/check_object.sh
+++ exercise/assets/check_object.sh
@@ -9,7 +9,7 @@ color="\e[0m"
 
 [ "$status" = 0 ] && color="\e[31m"
 
-msg = $(kubectl -n bookinfo get $1 $2 -o json | opa eval -I -d /root/exercise/checklist.rego "data.k8s.replica.count.apply_replicas_count" | 
+msg=$(kubectl -n bookinfo get $1 $2 -o json | opa eval -I -d /root/exercise/checklist.rego "data.k8s.replica.count.apply_replicas_count" | 
 jq -Mr '.result[0].expressions[0].value.msg')
 
 echo "$2:$color$msg\e[0m\n"
