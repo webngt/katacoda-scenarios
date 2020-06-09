824a9f551c6ec9cb4957021ffd737caf07995925
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-06-09 19:11:57 +0300

    fix equality test and bold formatting

--- exercise/assets/check_object.sh
+++ exercise/assets/check_object.sh
@@ -7,11 +7,11 @@ jq -Mr '.result[0].expressions[0].value.status')
 # color
 color="\e[92m"
 
-[ "$status" = 0 ] && color="\e[31m"
+[ "$status" = "0" ] && color="\e[31m"
 
 msg=$(kubectl -n bookinfo get $1 $2 -o json | opa eval -I -d /root/exercise/checklist.rego "data.k8s.replica.count.apply_replicas_count" | 
 jq -Mr '.result[0].expressions[0].value.msg')
 
 #echo -e "\e1mproductpage-v1:\e[0m\e[91m Проверка на кол-во реплик не пройдена. Реплик: 1\e[0m\n"
-echo -e "\e1m$2:\e[0m $color$msg\e[0m\n"
+echo -e "\e[1m$2:\e[0m $color$msg\e[0m"
 
