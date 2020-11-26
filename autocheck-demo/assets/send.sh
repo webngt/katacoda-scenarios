d7d48831efe9a798c0a6b4614e9d76a2d7a01b9f
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-11-27 02:05:21 +0300

    less verbose curl, fix read -p prompt

--- autocheck-demo/assets/send.sh
+++ autocheck-demo/assets/send.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read -p "Enter fullname: " token
+read -p "Enter token: " token
 
 kubectl -n bookinfo get deployments -o json | \
 opa eval -f pretty -I -d /tmp/deployment.rego "data.k8s.deployment.policy" | \
-curl -v -d @- -H "Authorization: Bearer $token" -H "Content-Type: application/json" -X POST https://d5d934qvpovuk7t4g195.apigw.yandexcloud.net/check
+curl -d @- -H "Authorization: Bearer $token" -H "Content-Type: application/json" -X POST https://d5d934qvpovuk7t4g195.apigw.yandexcloud.net/check
