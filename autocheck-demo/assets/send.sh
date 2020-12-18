3c0872be64cb6d171282882ccd21cbcae16e0b92
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-12-18 14:11:53 +0300

    retry with logging

--- autocheck-demo/assets/send.sh
+++ autocheck-demo/assets/send.sh
@@ -2,6 +2,22 @@
 
 read -p "Enter token: " token
 
+result=$(kubectl -n bookinfo get deployments -o json | \
+opa eval -f pretty -I -d /tmp/deployment.rego "data.k8s.deployment.policy" | \
+curl -s -d @- -w %{http_code}  \
+-H "Authorization: Bearer $token" -H "Content-Type: application/json" \
+-X POST https://bootcamp.pcbltools.ru/services/result-service/save)
+
+[ "$result" = "200" ] && echo -en "\e[32mOK\e[0m\n" && exit 0
+
+echo -en "\e[91mCould not send results. Try again with logging enabled. Copy logs from /root/exercise/send.log and send them to our support.\e[0m\n"
+
+read -p "Enter token: " token
+
 kubectl -n bookinfo get deployments -o json | \
 opa eval -f pretty -I -d /tmp/deployment.rego "data.k8s.deployment.policy" | \
-curl -d @- -H "Authorization: Bearer $token" -H "Content-Type: application/json" -X POST https://d5d934qvpovuk7t4g195.apigw.yandexcloud.net/check
+curl -s -v -d @-  \
+-H "Authorization: Bearer $token" -H "Content-Type: application/json" \
+-X POST https://bootcamp.pcbltools.ru/services/result-service/save > /tmp/send.log 2>&1
+
+cp /tmp/send.log /root/exercise
