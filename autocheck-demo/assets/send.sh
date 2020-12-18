250d8ed2177cfa55e5e25455191b3f45fe4082cc
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-12-18 15:39:58 +0300

    json variable

--- autocheck-demo/assets/send.sh
+++ autocheck-demo/assets/send.sh
@@ -2,15 +2,18 @@
 
 read -p "Enter token: " token
 
-result=$(kubectl -n bookinfo get deployments -o json | \
-opa eval -f pretty -I -d /tmp/deployment.rego "data.k8s.deployment.policy" | \
+# change json variable value if you want to have custom json output
+json=$(kubectl -n bookinfo get deployments -o json | \
+opa eval -f pretty -I -d /tmp/deployment.rego "data.k8s.deployment.policy")
+
+result=$(echo $json | \
 curl -s -d @- -w %{http_code}  \
 -H "Authorization: Bearer $token" -H "Content-Type: application/json" \
 -X POST https://bootcamp.pcbltools.ru/services/result-service/save)
 
 [ "$result" = "200" ] && echo -en "\e[32mOK\e[0m\n" && exit 0
 
-echo -en "\e[91mCould not send results. Try again with logging enabled. Copy logs from /root/exercise/send.log and send them to our support.\e[0m\n"
+echo -en "\e[91mCould not send results. Try again with logging enabled. Copy logs from /root/exercise/send.log and send them to our support team.\e[0m\n"
 
 read -p "Enter token: " token
 
