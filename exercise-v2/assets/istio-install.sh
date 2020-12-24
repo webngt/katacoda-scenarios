df3fb279405e8028096b5d728ace3453398a9d11
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-12-25 00:59:34 +0300

    do not apply traffic policy

--- exercise-v2/assets/istio-install.sh
+++ exercise-v2/assets/istio-install.sh
@@ -59,11 +59,11 @@ sleep 10
 # patch ingress gateway
 kubectl -n istio-system patch service istio-ingressgateway -p "$(cat /tmp/node-port.yaml)"
 kubectl -n istio-system patch --type="merge" service istio-ingressgateway -p "$(cat /tmp/immutable-ports.yaml)"
-kubectl -n istio-system patch service istio-ingressgateway -p "$(cat /tmp/traffic-policy.yaml)"
-kubectl -n istio-system patch deployment istio-ingressgateway -p "$(cat /tmp/antiaffinity.yaml)"
+#kubectl -n istio-system patch service istio-ingressgateway -p "$(cat /tmp/traffic-policy.yaml)"
+#kubectl -n istio-system patch deployment istio-ingressgateway -p "$(cat /tmp/antiaffinity.yaml)"
 
 
-kubectl -n istio-system scale deployment istio-ingressgateway  --replicas=2
+#kubectl -n istio-system scale deployment istio-ingressgateway  --replicas=2
 
 echo "Check istio install..."
 sleep 10
