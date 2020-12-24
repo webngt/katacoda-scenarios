bbcb3f38b246b59dc73f925cfc779bcb447b3ffc
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-12-24 15:52:00 +0300

    fix ensure_label

--- exercise-v2/assets/istio-install.sh
+++ exercise-v2/assets/istio-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-function ensure_label {
-    while [ "$(kubectl -n $2 wait --for=condition=ContainersReady --timeout=5m pods -l $1)" = "error: no matching resources found"]; do
+ensure_label() {
+    while [ "$(kubectl -n $2 wait --for=condition=ContainersReady --timeout=5m pods -l $1)" = "error: no matching resources found" ]; do
         echo "Retry..."
         sleep 20
     done
@@ -13,17 +13,31 @@ while [ "$(kubectl get pods --all-namespaces)" = "No resources found" ]; do
     sleep 10
 done
 
-ensure_label("component=etcd", "kube-system")
-ensure_label("component=kube-apiserver", "kube-system")
-ensure_label("component=kube-controller-manager", "kube-system")
-ensure_label("component=kube-scheduler", "kube-system")
-ensure_label("app=flannel", "kube-system")
-ensure_label("name=kube-keepalived-vip", "kube-system")
-ensure_label("k8s-app=kube-dns", "kube-system")
+echo "Wait for core cluster containers to be ready..."
+
+ensure_label "component=etcd" "kube-system"
+ensure_label "component=kube-apiserver" "kube-system"
+ensure_label "component=kube-controller-manager" "kube-system"
+ensure_label "component=kube-scheduler" "kube-system"
+ensure_label "app=flannel" "kube-system"
+ensure_label "name=kube-keepalived-vip" "kube-system"
+ensure_label "k8s-app=kube-dns" "kube-system"
 
 
 # untaint control plane
 kubectl taint nodes -l node-role.kubernetes.io/master node-role.kubernetes.io/master-
+
+# cni workaround
+ssh -o "StrictHostKeyChecking no" node01 'ip link set cni0 down'
+ip link set cni0 down
+
+kubectl scale deployment coredns --replicas=0 -n kube-system
+sleep 20
+
+kubectl scale deployment coredns --replicas=2 -n kube-system
+
+ensure_label "k8s-app=kube-dns", "kube-system"
+
 
 # install opa
 curl -sS -L -o opa https://openpolicyagent.org/downloads/latest/opa_linux_amd64
@@ -37,32 +51,11 @@ export ISTIO_VERSION=1.8.1
 export PATH=$HOME/istio-$ISTIO_VERSION/bin:$PATH
 [ ! -d "$HOME/exercise" ] && mkdir $HOME/exercise
 
-# cni workaround
-ssh -o "StrictHostKeyChecking no" node01 'ip link set cni0 down'
-ip link set cni0 down
-
-kubectl scale deployment coredns --replicas=0 -n kube-system
-sleep 20
-kubectl scale deployment coredns --replicas=2 -n kube-system
-
-ensure_label("k8s-app=kube-dns", "kube-system")
-
-while [ "$(kubectl get pods -l app!=katacoda-cloud-provider -n kube-system -o=jsonpath='{.items[*].status.conditions[?(@.status == "False")].status}')" != "" ]; do 
-    echo "Ensure k8s is properly initialized..."
-    sleep 10
-done
-
-
-
 istioctl install --set profile=demo --readiness-timeout='10m0s'
 sleep 10
 
 # scale down istio ingress
 kubectl -n istio-system scale deployment istio-ingressgateway  --replicas=0
-while [ "$(kubectl get pods -l app=istio-ingressgateway -n istio-system -o=jsonpath='{.items[*].status.conditions[?(@.status == "False")].status}')" != "" ]; do 
-    echo "Scaling down istio-ingressgateway...."
-    sleep 10
-done
 
 # patch ingress gateway
 kubectl -n istio-system patch service istio-ingressgateway -p "$(cat /tmp/node-port.yaml)"
@@ -72,12 +65,12 @@ kubectl -n istio-system patch deployment istio-ingressgateway -p "$(cat /tmp/ant
 
 
 kubectl -n istio-system scale deployment istio-ingressgateway  --replicas=2
-while [ "$(kubectl get pods -n istio-system -o=jsonpath='{.items[*].status.conditions[?(@.status == "False")].status}')" != "" ]; do 
-    echo "Scaling up istio-ingressgateway...."
-    sleep 10
-done
 
-kubectl get pods -n istio-system
+echo "Check istio install..."
+sleep 10
+
+kubectl -n istio-system wait --for=condition=ContainersReady --timeout=60s --all pods
+
 echo "Done."
 
 
